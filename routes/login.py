0222b5b32ebcb7594cd63b4c67aac1419944ed9f
sb48 <sb48@sanger.ac.uk>
2017-07-21 11:05:16 +0100

    Login now sets a user id (currently just the test id) Login now removes cookies if not logged in.

--- routes/login.py
+++ routes/login.py
@@ -1,3 +1,4 @@
+from db import User
 from aiohttp import web
 
 
@@ -11,13 +12,22 @@ async def login(request):
     user_type = post_req["type"]
     response = web.Response(text=user_type)
     response.set_cookie("user_type", user_type)
+    logged_in = True
     if user_type == "admin":
         for perm in next(iter(request.app["permissions"].values())):
             response.set_cookie(perm, True)
     elif user_type == "":
         for perm in next(iter(request.app["permissions"].values())):
-            response.set_cookie(perm, False)
+            response.del_cookie(perm)
+        logged_in = False
+        response.del_cookie("user_type")
     else:
         for key, value in request.app["permissions"][user_type].items():
             response.set_cookie(key, value)
+    if logged_in:
+        session = request.app["session"]
+        user = session.query(User).first()
+        response.set_cookie("user_id", user.id)
+    else:
+        response.del_cookie("user_id")
     return response
