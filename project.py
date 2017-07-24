64fc7b574ceeb1d6dfc8f6fbb66c6ad8ac380f14
sb48 <sb48@sanger.ac.uk>
2017-07-24 12:12:29 +0100

    Add a list of all the projects owned by you on dashboard Made supervisor optional in single_project_overview Add get_projects_user

--- project.py
+++ project.py
@@ -1,5 +1,6 @@
 from sqlalchemy import desc
-from db import ProjectGroup
+from permissions import is_user_id
+from db import ProjectGroup, Project
 
 
 def get_most_recent_group(session):
@@ -33,3 +34,18 @@ def get_series(session, series):
     :return ProjectGroup:
     """
     return session.query(ProjectGroup).filter(ProjectGroup.series == series).order_by(ProjectGroup.part).all()
+
+
+def get_projects_user(request, user_id):
+    session = request.app["session"]
+    cookies = request.cookies
+    projects = session.query(Project).filter_by(supervisor=user_id).all()
+    read_only_map = {}
+    rtn = {}
+    for project in projects:
+        if project.group not in read_only_map:
+            read_only_map[project.group] = session.query(ProjectGroup).filter_by(id=project.group).first().read_only
+            rtn[project.group] = []
+        project.read_only = read_only_map[project.group] or not is_user_id(cookies, project.supervisor)
+        rtn[project.group].append(project)
+    return (rtn[key] for key in sorted(rtn.keys(), reverse=True))
--- routes/user_page.py
+++ routes/user_page.py
@@ -1,5 +1,5 @@
 from aiohttp_jinja2 import template
-from project import get_most_recent_group, get_group, get_series
+from project import get_most_recent_group, get_projects_user
 
 
 @template("user_page.jinja2")
@@ -7,7 +7,9 @@ async def user_page(request):
     cookies = request.cookies
     session = request.app["session"]
     group = get_most_recent_group(session)
-
     rtn = {"can_edit": not group.read_only}
+    if cookies["create_projects"] == "True":
+        rtn["series_list"] = get_projects_user(request, int(cookies["user_id"]))
+
     rtn.update(cookies)
     return rtn
