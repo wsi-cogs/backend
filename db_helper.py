9f6f519bf4667abef3d2cf12d80638a59c532214
sb48 <sb48@sanger.ac.uk>
2017-08-09 11:55:36 +0100

    Fix bug in get_projects_user where only one project per rotation would be returned

--- db_helper.py
+++ db_helper.py
@@ -51,16 +51,16 @@ def get_projects_user(request, user_id):
     read_only_map = {}
     rtn = {}
     for project in projects:
-        if project.group not in read_only_map:
+        if project.group_id not in read_only_map:
             read_only_map[project.group_id] = project.group.read_only
             rtn[project.group_id] = []
         project.read_only = read_only_map[project.group_id] or not is_user(cookies, project.supervisor)
         rtn[project.group_id].append(project)
-    return (rtn[key] for key in sorted(rtn.keys(), reverse=True))
+    return [rtn[key] for key in sorted(rtn.keys(), reverse=True)]
 
 
 def get_project_name(session, project_name):
-    return session.query(Project).filter_by(title=project_name).first()
+    return session.query(Project).filter_by(title=project_name).order_by(Project.id.desc()).first()
 
 
 def get_project_id(session, project_id):
