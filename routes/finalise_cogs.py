75e8b0b471c218bffdb2a088e6b60b05b893cba7
sb48 <sb48@sanger.ac.uk>
2017-08-24 12:29:43 +0100

    Don't have to choose CoGS markers at choose project time

--- routes/finalise_cogs.py
+++ routes/finalise_cogs.py
@@ -21,17 +21,19 @@ async def on_submit_cogs(request):
     session = request.app["session"]
     supervisors = defaultdict(list)
     group = get_most_recent_group(session)
+    for project in group.projects:
+        if project.student:
+            student = project.student
+            choice = (student.first_option, student.second_option, student.third_option).index(project)
+            student.priority += choice ** 2
+            student.first_option = None
+            student.second_option = None
+            student.third_option = None
+            await send_user_email(request.app, student, "project_selected_student", project=project)
+            supervisors[project.supervisor].append(project)
     for project_id, cogs_member_id in (await request.post()).items():
         project = get_project_id(session, int(project_id))
         project.cogs_marker_id = int(cogs_member_id)
-        student = project.student
-        choice = (student.first_option, student.second_option, student.third_option).index(project)
-        student.priority += choice ** 2
-        student.first_option = None
-        student.second_option = None
-        student.third_option = None
-        await send_user_email(request.app, student, "project_selected_student", project=project)
-        supervisors[project.supervisor].append(project)
     for supervisor in get_users_with_permission(request.app, "create_projects"):
         projects = [project for project in sum(get_projects_supervisor(request, supervisor.id), [])
                     if project.group == group]
