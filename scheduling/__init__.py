4f635c9a90bfa74bb489db1a8a84c5e36d475e5b
sb48 <sb48@sanger.ac.uk>
2017-09-12 08:47:01 +0100

    Set job misfire time to 31 days so the system will still work if it's inactive for a while

--- scheduling/__init__.py
+++ scheduling/__init__.py
@@ -8,17 +8,19 @@ from scheduling.deadlines import deadline_scheduler
 def setup(app: Application) -> None:
     jobstore = SQLAlchemyJobStore(engine=app["db"])
     jobstores = {"default": jobstore}
-    scheduler = AsyncIOScheduler(jobstores=jobstores)
+    scheduler = AsyncIOScheduler(jobstores=jobstores,
+                                 job_defaults={"misfire_grace_time": 31 * 24 * 60 * 60})
 
     scheduler.start()
     scheduler.print_jobs()
     app["scheduler"] = scheduler
     # TODO: Remove
     scheduler.remove_all_jobs()
-    from db_helper import get_most_recent_group
-    from datetime import datetime, timedelta
-    deadlines.schedule_deadline(app,
-                                get_most_recent_group(app["session"]),
-                                "student_choice",
-                                datetime.now()+timedelta(seconds=15),
-                                pester_users=False)
+
+    #from db_helper import get_most_recent_group
+    #from datetime import datetime, timedelta
+    #deadlines.schedule_deadline(app,
+    #                            get_most_recent_group(app["session"]),
+    #                            "student_choice",
+    #                            datetime.now()+timedelta(seconds=15),
+    #                            pester_users=False)
