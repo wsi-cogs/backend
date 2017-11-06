4a7722aedd39c279b92863a45dfce418fdcbb5e6
Christopher Harrison <ch12@sanger.ac.uk>
2017-11-06 16:02:52 +0000

    Tidied up entrypoint

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
-import os
+"""
+Copyright (c) 2017 Genome Research Ltd.
+
+Authors:
+* Simon Beal <sb48@sanger.ac.uk>
+* Christopher Harrison <ch12@sanger.ac.uk>
+
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from os import path
 
 import aiohttp_jinja2
 from aiohttp import web
@@ -13,17 +34,11 @@ from routes import setup_routes
 from scheduling import setup as setup_scheduler
 
 
-def main() -> None:
-    """
-    Launch the app
-
-    :return:
-    """
-    global app
+if __name__ == '__main__':
     app = web.Application()
     setup_routes(app)
 
-    conf = load_config(os.path.join("config", "config.yaml"))
+    conf = load_config(path.join("config", "config.yaml"))
     app["db_config"] = conf["db"]
     app["deadlines"] = conf["deadlines"]
     app["login_db"] = conf["login_db"]
@@ -46,6 +61,3 @@ def main() -> None:
     web.run_app(app,
                 host=conf["webserver"]["host"],
                 port=conf["webserver"]["port"])
-
-if __name__ == '__main__':
-    main()
