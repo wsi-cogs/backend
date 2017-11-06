c22213e4b49a6e0717066e4f8ba3d94554ddd480
Christopher Harrison <ch12@sanger.ac.uk>
2017-11-06 16:24:57 +0000

    Tidied up configuration loading

---
+++ config/load.py
@@ -0,0 +1,47 @@
+"""
+Copyright (c) 2017 Genome Research Ltd.
+
+Authors:
+* Simon Beal <sb48@sanger.ac.uk>
+* Christopher Harrison <ch12@sanger.ac.uk>
+
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from collections import OrderedDict
+
+import yaml
+
+
+def load_config(conf_path):
+    """
+    Load the configuration YAML from conf_path
+
+    :param conf_path:
+    :return:
+    """
+    with open(conf_path) as stream:
+        # YAML to OrderedDict (c) 2014 Thomas Gläßle
+        # https://stackoverflow.com/a/21912744/3398583
+        class OrderedLoader(yaml.SafeLoader):
+            pass
+
+        def construct_mapping(loader, node):
+            loader.flatten_mapping(node)
+            return OrderedDict(loader.construct_pairs(node))
+
+        OrderedLoader.add_constructor(yaml.resolver.BaseResolver.DEFAULT_MAPPING_TAG,
+                                      construct_mapping)
+
+        return yaml.load(stream, OrderedLoader)
