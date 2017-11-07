dcc0b5cec88da2723505071b9fe82c313a00708d
Christopher Harrison <ch12@sanger.ac.uk>
2017-11-07 13:36:29 +0000

    Refactored database service functions

--- cogs/db/service.py
+++ cogs/db/service.py
@@ -19,7 +19,13 @@ You should have received a copy of the GNU Affero General Public License
 along with this program. If not, see <https://www.gnu.org/licenses/>.
 """
 
+from datetime import datetime
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 from cogs.common.types import Application, DBSession
+from . import functions, models
 
 
 async def start(app:Application) -> DBSession:
@@ -30,52 +36,44 @@ async def start(app:Application) -> DBSession:
     :param app:
     :return session:
     """
-    import db_helper
-
     conf = app["config"]["db"]
-    engine = create_engine(f"postgresql://{conf['user']}:{conf['password']}@{conf['host']}:{conf['port']}/{conf['name']}")
+    engine = create_engine("postgresql://{user}:{password}@{host}:{port}/{name}".format(**conf))
 
-    if "reset_db" in sys.argv:
-        print("Resetting database...")
-        for table in Base.metadata.tables.values():
-            try:
-                engine.execute(f"DROP TABLE {table} CASCADE;")
-            except ProgrammingError:
-                try:
-                    engine.execute(f'DROP TABLE "{table}" CASCADE;')
-                except ProgrammingError:
-                    pass
-
-    Base.metadata.create_all(engine)
+    models.Base.metadata.create_all(engine)
     app["db"] = engine
 
     Session = sessionmaker(bind=engine)
     app["session"] = session = Session()
 
-    for template in app["config"]["misc"]["email_whitelist"]:
-        if db_helper.get_template_name(session, template) is None:
-            session.add(EmailTemplate(name=template,
-                                      subject=f"Subject for {template}",
-                                      content=f"Content for {template}"))
+    # FIXME? Do we really want to populate the database with these
+    # defaults if it's empty?
 
-    if not db_helper.get_all_users(session):
-        print("No users found. Adding admins. (sb48 & ca3)")
-        session.add(User(name="Simon Beal", email="sb48@sanger.ac.uk", user_type="grad_office", priority=0))
-        session.add(User(name="Carl Anderson", email="ca3@sanger.ac.uk", user_type="grad_office", priority=0))
-    if not db_helper.get_all_groups(session):
+    for template in app["config"]["misc"]["email_whitelist"]:
+        if not functions.get_template_name(session, template):
+            session.add(models.EmailTemplate(name=template,
+                                             subject=f"Subject for {template}",
+                                             content=f"Content for {template}"))
+
+    if not functions.get_all_users(session):
+        print("No users found. Adding admins.")
+        _admin_args = {"user_type": "grad_office", "priority": 0}
+        session.add(models.User(name="Simon Beal",    email="sb48@sanger.ac.uk", **_admin_args))
+        session.add(models.User(name="Carl Anderson", email="ca3@sanger.ac.uk",  **_admin_args))
+
+    if not functions.get_all_groups(session):
         print("No groups found. Adding rotation 1 2017.")
-        session.add(ProjectGroup(series=2017,
-                                 part=1,
-                                 supervisor_submit=datetime.strptime("18/07/2017", "%d/%m/%Y"),
-                                 student_invite=datetime.strptime("08/08/2017", "%d/%m/%Y"),
-                                 student_choice=datetime.strptime("30/08/2017", "%d/%m/%Y"),
-                                 student_complete=datetime.strptime("20/12/2017", "%d/%m/%Y"),
-                                 marking_complete=datetime.strptime("15/01/2018", "%d/%m/%Y"),
-                                 student_viewable=True,
-                                 student_choosable=True,
-                                 student_uploadable=False,
-                                 can_finalise=True,
-                                 read_only=False))
+        session.add(models.ProjectGroup(series=2017,
+                                        part=1,
+                                        supervisor_submit=datetime.strptime("18/07/2017", "%d/%m/%Y"),
+                                        student_invite=datetime.strptime("08/08/2017", "%d/%m/%Y"),
+                                        student_choice=datetime.strptime("30/08/2017", "%d/%m/%Y"),
+                                        student_complete=datetime.strptime("20/12/2017", "%d/%m/%Y"),
+                                        marking_complete=datetime.strptime("15/01/2018", "%d/%m/%Y"),
+                                        student_viewable=True,
+                                        student_choosable=True,
+                                        student_uploadable=False,
+                                        can_finalise=True,
+                                        read_only=False))
 
     session.commit()
     return session
