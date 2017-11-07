532f4ff4c17d20842cc23ad03ed6eaa19fe618ff
Christopher Harrison <ch12@sanger.ac.uk>
2017-11-07 14:52:02 +0000

    Refactored database models

--- cogs/db/models.py
+++ cogs/db/models.py
@@ -1,16 +1,30 @@
-import sys
-from datetime import datetime
+"""
+Copyright (c) 2017 Genome Research Ltd.
 
-from aiohttp.web import Application
-from sqlalchemy import create_engine, Integer, String, Column, Date, ForeignKey, Boolean
+Authors:
+* Simon Beal <sb48@sanger.ac.uk>
+* Christopher Harrison <ch12@sanger.ac.uk>
+
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from sqlalchemy import Integer, String, Column, Date, ForeignKey, Boolean
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
-from sqlalchemy.exc import ProgrammingError
-
-from type_hints import DBSession
+from sqlalchemy.orm import relationship
 
 
-def base_repr(self):
+def _base_repr(self):
     """
     Monkeypatch the Base object so it's `eval`able
 
@@ -19,95 +33,97 @@ def base_repr(self):
     """
     params = ", ".join("{}={}".format(column.key, repr(getattr(self, column.key)))
                        for column in self.__table__.columns)
+
     return f"{self.__class__.__name__}({params})"
 
-
 Base = declarative_base()
-Base.__repr__ = base_repr
+Base.__repr__ = _base_repr
 
 
 class ProjectGroup(Base):
-    __tablename__ = "project_groups"
-    id = Column(Integer, primary_key=True)
-    supervisor_submit = Column(Date)
-    student_invite = Column(Date)
-    student_choice = Column(Date)
-    student_complete = Column(Date)
-    marking_complete = Column(Date)
-    series = Column(Integer)
-    part = Column(Integer)
-    student_viewable = Column(Boolean)
-    student_choosable = Column(Boolean)
-    student_uploadable = Column(Boolean)
-    can_finalise = Column(Boolean)
-    # Can supervisors modify the projects in this group
-    read_only = Column(Boolean)
+    __tablename__          = "project_groups"
 
-    projects = relationship("Project")
+    id                     = Column(Integer, primary_key=True)
+    supervisor_submit      = Column(Date)
+    student_invite         = Column(Date)
+    student_choice         = Column(Date)
+    student_complete       = Column(Date)
+    marking_complete       = Column(Date)
+    series                 = Column(Integer)
+    part                   = Column(Integer)
+    student_viewable       = Column(Boolean)
+    student_choosable      = Column(Boolean)
+    student_uploadable     = Column(Boolean)
+    can_finalise           = Column(Boolean)
+    read_only              = Column(Boolean)  # Can supervisors modify the projects in this group
+
+    projects               = relationship("Project")
 
 
 class ProjectGrade(Base):
-    __tablename__ = "project_grades"
-    id = Column(Integer, primary_key=True)
-    grade_id = Column(Integer)
-    good_feedback = Column(String)
-    bad_feedback = Column(String)
-    general_feedback = Column(String)
+    __tablename__          = "project_grades"
+
+    id                     = Column(Integer, primary_key=True)
+    grade_id               = Column(Integer)
+    good_feedback          = Column(String)
+    bad_feedback           = Column(String)
+    general_feedback       = Column(String)
 
 
 class Project(Base):
-    __tablename__ = "projects"
-    id = Column(Integer, primary_key=True)
-    title = Column(String)
-    small_info = Column(String)
-    abstract = Column(String)
-    is_computational = Column(Boolean)
-    is_wetlab = Column(Boolean)
-    programmes = Column(String)
+    __tablename__          = "projects"
 
-    uploaded = Column(Boolean)
-    grace_passed = Column(Boolean)
+    id                     = Column(Integer, primary_key=True)
+    title                  = Column(String)
+    small_info             = Column(String)
+    abstract               = Column(String)
+    is_computational       = Column(Boolean)
+    is_wetlab              = Column(Boolean)
+    programmes             = Column(String)
 
-    supervisor_id = Column(Integer, ForeignKey("users.id", ondelete="SET NULL"))
-    cogs_marker_id = Column(Integer, ForeignKey("users.id", ondelete="SET NULL"))
-    student_id = Column(Integer, ForeignKey("users.id", ondelete="SET NULL"))
-    group_id = Column(Integer, ForeignKey(ProjectGroup.id, ondelete="CASCADE"))
+    uploaded               = Column(Boolean)
+    grace_passed           = Column(Boolean)
+
+    supervisor_id          = Column(Integer, ForeignKey("users.id", ondelete="SET NULL"))
+    cogs_marker_id         = Column(Integer, ForeignKey("users.id", ondelete="SET NULL"))
+    student_id             = Column(Integer, ForeignKey("users.id", ondelete="SET NULL"))
+    group_id               = Column(Integer, ForeignKey(ProjectGroup.id, ondelete="CASCADE"))
 
     supervisor_feedback_id = Column(Integer, ForeignKey(ProjectGrade.id, ondelete="CASCADE"))
-    cogs_feedback_id = Column(Integer, ForeignKey(ProjectGrade.id, ondelete="CASCADE"))
+    cogs_feedback_id       = Column(Integer, ForeignKey(ProjectGrade.id, ondelete="CASCADE"))
 
-    supervisor = relationship("User", foreign_keys=supervisor_id, post_update=True)
-    cogs_marker = relationship("User", foreign_keys=cogs_marker_id, post_update=True)
-    student = relationship("User", foreign_keys=student_id, post_update=True)
-    group = relationship(ProjectGroup, foreign_keys=group_id)
-    supervisor_feedback = relationship(ProjectGrade, foreign_keys=supervisor_feedback_id)
-    cogs_feedback = relationship(ProjectGrade, foreign_keys=cogs_feedback_id)
+    supervisor             = relationship("User",       foreign_keys=supervisor_id, post_update=True)
+    cogs_marker            = relationship("User",       foreign_keys=cogs_marker_id, post_update=True)
+    student                = relationship("User",       foreign_keys=student_id, post_update=True)
+    group                  = relationship(ProjectGroup, foreign_keys=group_id)
+    supervisor_feedback    = relationship(ProjectGrade, foreign_keys=supervisor_feedback_id)
+    cogs_feedback          = relationship(ProjectGrade, foreign_keys=cogs_feedback_id)
 
 
 class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
-    email = Column(String)
-    user_type = Column(String)
+    __tablename__          = "users"
 
-    priority = Column(Integer)
+    id                     = Column(Integer, primary_key=True)
+    name                   = Column(String)
+    email                  = Column(String)
+    user_type              = Column(String)
 
-    first_option_id = Column(Integer, ForeignKey(Project.id, ondelete="SET NULL"))
-    second_option_id = Column(Integer, ForeignKey(Project.id, ondelete="SET NULL"))
-    third_option_id = Column(Integer, ForeignKey(Project.id, ondelete="SET NULL"))
+    priority               = Column(Integer)
 
-    first_option = relationship(Project, foreign_keys=first_option_id, post_update=True)
-    second_option = relationship(Project, foreign_keys=second_option_id, post_update=True)
-    third_option = relationship(Project, foreign_keys=third_option_id, post_update=True)
+    first_option_id        = Column(Integer, ForeignKey(Project.id, ondelete="SET NULL"))
+    second_option_id       = Column(Integer, ForeignKey(Project.id, ondelete="SET NULL"))
+    third_option_id        = Column(Integer, ForeignKey(Project.id, ondelete="SET NULL"))
+
+    first_option           = relationship(Project, foreign_keys=first_option_id, post_update=True)
+    second_option          = relationship(Project, foreign_keys=second_option_id, post_update=True)
+    third_option           = relationship(Project, foreign_keys=third_option_id, post_update=True)
 
 
 class EmailTemplate(Base):
-    __tablename__ = "email_templates"
-    id = Column(Integer, primary_key=True)
-    name = Column(String)
+    __tablename__          = "email_templates"
 
-    subject = Column(String)
-    content = Column(String)
+    id                     = Column(Integer, primary_key=True)
+    name                   = Column(String)
 
-
+    subject                = Column(String)
+    content                = Column(String)
