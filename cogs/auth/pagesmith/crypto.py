f2740a494f6df9a48f315cd39eb080bdaf939444
Christopher Harrison <ch12@sanger.ac.uk>
2017-11-08 17:02:24 +0000

    Pagesmith authentication implementation

--- cogs/auth/pagesmith/crypto.py
+++ cogs/auth/pagesmith/crypto.py
@@ -1,13 +1,40 @@
-import base64
+"""
+Copyright (c) 2017 Genome Research Ltd.
+
+Authors:
+* Simon Beal <sb48@sanger.ac.uk>
+* Christopher Harrison <ch12@sanger.ac.uk>
+* Joshua Randall <jr17@sanger.ac.uk>
+
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+
 from hashlib import md5
 
-from aiohttp.web import Application
 from blowfish import Cipher
 
 
-class BlowfishDecryption:
-    def __init__(self, passphrase: bytes):
+class BlowfishCBCDecrypt:
+    """ Blowfish decryption in CBC mode with Perl compatibility """
+    def __init__(self, passphrase:bytes) -> None:
+        """
+        Constructor: Initialise the cipher with the key derived from the
+        passphrase in the same way Perl does it
 
+        :param passphrase:
+        :return:
+        """
         key = md5(passphrase).digest()
         while len(key) < 56:
             key += md5(key).digest()
@@ -15,14 +42,14 @@ class BlowfishDecryption:
 
         self.cipher = Cipher(key)
 
-    def decrypt(self, data: str):
-        blob = base64.b64decode(data + "==", "-_")
-        iv, data = blob[:8], blob[8:]
-        plaintext = b"".join(self.cipher.decrypt_cbc(data, iv))
-        no_to_remove = plaintext[-1]
-        plaintext = plaintext[8:-no_to_remove]
-        return plaintext
+    def decrypt(self, ciphertext:bytes) -> bytes:
+        """
+        Decrypt the ciphertext
 
-
-def init_blowfish(app: Application) -> None:
-    app.blowfish = BlowfishDecryption(app["config"]["login_db"]["blowfish_key"].encode())
+        :param ciphertext:
+        :return:
+        """
+        iv, data = ciphertext[:8], ciphertext[8:]
+        padded_plaintext = b"".join(self.cipher.decrypt_cbc(data, iv))
+        padding = int(padded_plaintext[-1])
+        return padded_plaintext[8:-padding]
---
+++ cogs/auth/pagesmith/pagesmith.py
@@ -0,0 +1,114 @@
+"""
+Copyright (c) 2017 Genome Research Ltd.
+
+Authors:
+* Christopher Harrison <ch12@sanger.ac.uk>
+
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import base64
+import json
+from datetime import datetime
+from typing import Dict, NamedTuple
+
+import MySQLdb as mysql
+
+from cogs.auth.abc import BaseAuthenticator
+from cogs.auth.exceptions import UnknownUserError
+from cogs.common.types import Cookies
+from .crypto import BlowfishCBCDecrypt
+from .exceptions import InvalidPagesmithUserCookie, NoPagesmithUserCookie
+
+
+class _AuthenticatedUser(NamedTuple):
+    email:str
+    expiry:datetime
+
+
+class PagesmithAuthenticator(BaseAuthenticator):
+    """ Pagesmith authentication """
+    def __init__(self, config:Dict) -> None:
+        """
+        Constructor: Set up necessary state for authentication,
+        including a cache of already-authenticated users
+
+        :param config:
+        :return:
+        """
+        self._cache:Dict[str, _AuthenticatedUser] = {}
+        self._db = mysql.connect(**config["database"])
+        self._crypto = BlowfishCBCDecrypt(config["cookie_key"])
+
+    def get_email_by_uuid(self, uuid:str) -> str:
+        """
+        Fetch the e-mail address by the given UUID from the Pagesmith DB
+
+        :param uuid:
+        :return:
+        """
+        with self._db as db:
+            with db.cursor() as cursor:
+                data = cursor.execute("""
+                    select content
+                    from   session
+                    where  type = 'User'
+                    and    session_key = %s;
+                """, (uuid,)).fetchone()
+
+        if not data:
+            raise UnknownUserError()
+
+        decrypted = self._crypto.decrypt(data[0][1:])
+        data_json = json.loads(decrypted)
+
+        return data_json["email"]
+
+    async def extract_identifier_from_source(self, cookies:Cookies) -> str:
+        """
+        Extract the e-mail address from the Pagesmith user cookie (or
+        cache, if available)
+
+        :param cookies:
+        :return:
+        """
+        try:
+            pagesmith_user = cookies["Pagesmith_User"].replace("%0A", "")
+        except KeyError:
+            raise NoPagesmithUserCookie()
+
+        # Get from cache, if available
+        if pagesmith_user in self._cache:
+            email, expiry = self._cache[pagesmith_user]
+            if expiry > datetime.now():
+                return email
+
+            # Invalidate expired logins
+            del self._cache[pagesmith_user]
+
+        try:
+            ciphertext = base64.b64decode(pagesmith_user.encode() + b"==", b"-_")
+            decrypted = self._crypto.decrypt(ciphertext)
+            _perm, uuid, _refresh, expiry, _ip = decrypted.split(b" ")
+
+            uuid = uuid.decode()
+            expiry = datetime.utcfromtimestamp(float(expiry))
+
+        except:
+            raise InvalidPagesmithUserCookie()
+
+        email = self.get_email_by_uuid(uuid)
+        self._cache[pagesmith_user] = _AuthenticatedUser(email, expiry)
+
+        return email
