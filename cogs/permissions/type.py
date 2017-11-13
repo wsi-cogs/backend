9dde773928ab819882744cc029def95123daa20a
Christopher Harrison <ch12@sanger.ac.uk>
2017-11-13 16:03:31 +0000

    Base permission type

--- cogs/permissions/type.py
+++ cogs/permissions/type.py
@@ -1,156 +1,238 @@
-from typing import Union, Collection, Callable, Set, Sequence, Optional
+"""
+Copyright (c) 2017 Genome Research Ltd.
 
-from aiohttp import web
-from aiohttp.web import Application
-from aiohttp.web_request import Request
-from aiohttp.web_response import Response
+Authors:
+* Simon Beal <sb48@sanger.ac.uk>
+* Christopher Harrison <ch12@sanger.ac.uk>
 
-from cogs.db import functions
-from cogs.db.models import Project, ProjectGroup, User
-from cogs.common.types import Cookies
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+#from typing import Union, Collection, Callable, Set, Sequence, Optional
+#
+#from aiohttp import web
+#from aiohttp.web import Application
+#from aiohttp.web_request import Request
+#from aiohttp.web_response import Response
+#
+#from cogs.db import functions
+#from cogs.db.models import Project, ProjectGroup, User
+#from cogs.common.types import Cookies
+
+import textwrap
+from typing import Dict, Type
+
+from cogs.common.constants import PERMISSIONS
 
 
-def get_permission_from_cookie(app: Application, cookies: Cookies, permission: str) -> bool:
+class _BaseRole(object):
+    """ Base role object """
+    _permissions:Dict[str, bool]
+
+    def __init__(self) -> None:
+        self._permissions = {}
+
+    def __eq__(self, other:"_BaseRole") -> bool:
+        """ Role equivalence """
+        return self.__class__ == other.__class__ \
+           and all(v == other._permissions[k]
+                   for k, v in self._permissions.items())
+
+    def __or__(self, other:"_BaseRole") -> "_BaseRole":
+        """ Logical disjunction of equivalent permissions """
+        assert self.__class__ == other.__class__
+        return self.__class__(**{k: v | other._permissions[k]
+                                 for k, v in self._permissions.items()})
+
+def _build_role(*permissions:str) -> Type[_BaseRole]:
     """
-    Given a cookie store, return if the user is allowed a given permission.
-
-    :param app:
-    :param app:
-    :param cookies:
-    :param permission:
-    :return:
-    """
-    user = functions.get_user_id(app, cookies)
-    if user is None:
-        return False
-    return permission in get_user_permissions(app, user)
-
-
-def view_only(permissions: Union[Collection[str], str]) -> Callable:
-    """
-    Returns a 403 status error is the client is not authorised to view the content.
-    Otherwise allows the function to be called as normal.
+    Build a role class with a constructor taking boolean arguments
+    matching the specified permissions, with respective, read-only
+    properties
 
     :param permissions:
     :return:
     """
-    def decorator(func: Callable) -> Callable:
-        def inner(request: Request) -> Response:
-            nonlocal permissions
-            if isinstance(permissions, str):
-                permissions = (permissions, )
-            for permission in permissions:
-                if not get_permission_from_cookie(request.app, request.cookies, permission):
-                    return web.Response(status=403, text="Permission Denied")
-            return func(request)
-        return inner
-    return decorator
+    assert permissions
 
+    # Define constructor
+    src = """
+    class Role(_BaseRole):
+        def __init__(self, *, {init_params}) -> None:
+            super().__init__()
+            self._permissions = {{ {param_dict} }}
+    """.format(
+        init_params = ", ".join(map(lambda p: f"{p}:bool", permissions)),
+        param_dict  = ", ".join(map(lambda p: f"\"{p}\": {p}", permissions))
+    )
 
-def is_user(app, cookies, user: User) -> bool:
-    """
-    Return if the currently logged in user is the passed user
+    # Define properties
+    for p in permissions:
+        src += """
+        @property
+        def {p}(self) -> bool:
+            return self._permissions["{p}"]
+        """.format(p=p)
 
-    :param app:
-    :param cookies:
-    :param user:
-    :return:
-    """
-    return functions.get_user_cookies(app, cookies) == user.id
+    # Oh god, now I'm going to need to take a shower :P
+    namespace = {"_BaseRole": _BaseRole}
+    exec(textwrap.dedent(src), namespace)
+    return namespace["Role"]
 
+Role = _build_role(*PERMISSIONS)
 
-def can_view_group(request: Request, group: ProjectGroup) -> bool:
-    """
-    Can the logged in user view `group`?
-
-    :param request:
-    :param group:
-    :return:
-    """
-    cookies = request.cookies
-    if functions.get_user_cookies(request.app, cookies) == -1:
-        return False
-    if get_permission_from_cookie(request.app, cookies, "view_projects_predeadline"):
-        return True
-    return group.student_viewable
-
-
-def can_choose_project(app: Application, cookies: Cookies, project: Project) -> bool:
-    """
-    Can the logged in user choose `project`?
-
-    :param app:
-    :param cookies:
-    :param project:
-    :return:
-    """
-    if get_permission_from_cookie(app, cookies, "join_projects"):
-        if project.group.student_choosable:
-            if project.group.part != 3:
-                return True
-            done_projects = functions.get_student_projects(app, cookies)
-            done_projects.append(project)
-            done_computational = any(project.is_computational for project in done_projects)
-            done_wetlab = any(project.is_wetlab for project in done_projects)
-            return done_computational and done_wetlab
-    return False
-
-
-def value_set(column: str, predicate: Callable=lambda value: value, response: str="Permission Denied"):
-    """
-    Only complete the request if `predicate`(most_recent_group(`column`)) returns a truthy value
-
-    :param column:
-    :param predicate:
-    :param response:
-    :return:
-    """
-    def decorator(func):
-        def inner(request):
-            nonlocal column
-            session = request.app["session"]
-            group = functions.get_most_recent_group(session)
-            if predicate(getattr(group, column)):
-                return func(request)
-            return web.Response(status=403, text=response)
-        return inner
-    return decorator
-
-
-def get_users_with_permission(app: Application, permission_names: Union[str, Sequence[str]]) -> Set:
-    """
-    Return the users who have any of the permissions in `permission_names`
-
-    :param app:
-    :param permission_names:
-    :return:
-    """
-    rtn = set()
-    if isinstance(permission_names, str):
-        permission_names = (permission_names,)
-    for user in functions.get_all_users(app["session"]):
-        perms = get_user_permissions(app, user)
-        for permission_name in permission_names:
-            if permission_name in perms:
-                rtn.add(user)
-    return rtn
-
-
-def get_user_permissions(app: Application, user: Optional[User]) -> Set:
-    """
-    Return the permissions `user` has
-
-    :param app:
-    :param user:
-    :return:
-    """
-    if user is None:
-        return set()
-    user_types = user.user_type.split("|")
-    permissions = set()
-    for user_type in user_types:
-        if user_type:
-            for permission, value in app["config"]["permissions"][user_type].items():
-                if value:
-                    permissions.add(permission)
-    return permissions
+## def get_permission_from_cookie(app: Application, cookies: Cookies, permission: str) -> bool:
+##     """
+##     Given a cookie store, return if the user is allowed a given permission.
+## 
+##     :param app:
+##     :param app:
+##     :param cookies:
+##     :param permission:
+##     :return:
+##     """
+##     user = functions.get_user_id(app, cookies)
+##     if user is None:
+##         return False
+##     return permission in get_user_permissions(app, user)
+## 
+## 
+## def view_only(permissions: Union[Collection[str], str]) -> Callable:
+##     """
+##     Returns a 403 status error is the client is not authorised to view the content.
+##     Otherwise allows the function to be called as normal.
+## 
+##     :param permissions:
+##     :return:
+##     """
+##     def decorator(func: Callable) -> Callable:
+##         def inner(request: Request) -> Response:
+##             nonlocal permissions
+##             if isinstance(permissions, str):
+##                 permissions = (permissions, )
+##             for permission in permissions:
+##                 if not get_permission_from_cookie(request.app, request.cookies, permission):
+##                     return web.Response(status=403, text="Permission Denied")
+##             return func(request)
+##         return inner
+##     return decorator
+## 
+## 
+## def is_user(app, cookies, user: User) -> bool:
+##     """
+##     Return if the currently logged in user is the passed user
+## 
+##     :param app:
+##     :param cookies:
+##     :param user:
+##     :return:
+##     """
+##     return functions.get_user_cookies(app, cookies) == user.id
+## 
+## 
+## def can_view_group(request: Request, group: ProjectGroup) -> bool:
+##     """
+##     Can the logged in user view `group`?
+## 
+##     :param request:
+##     :param group:
+##     :return:
+##     """
+##     cookies = request.cookies
+##     if functions.get_user_cookies(request.app, cookies) == -1:
+##         return False
+##     if get_permission_from_cookie(request.app, cookies, "view_projects_predeadline"):
+##         return True
+##     return group.student_viewable
+## 
+## 
+## def can_choose_project(app: Application, cookies: Cookies, project: Project) -> bool:
+##     """
+##     Can the logged in user choose `project`?
+## 
+##     :param app:
+##     :param cookies:
+##     :param project:
+##     :return:
+##     """
+##     if get_permission_from_cookie(app, cookies, "join_projects"):
+##         if project.group.student_choosable:
+##             if project.group.part != 3:
+##                 return True
+##             done_projects = functions.get_student_projects(app, cookies)
+##             done_projects.append(project)
+##             done_computational = any(project.is_computational for project in done_projects)
+##             done_wetlab = any(project.is_wetlab for project in done_projects)
+##             return done_computational and done_wetlab
+##     return False
+## 
+## 
+## def value_set(column: str, predicate: Callable=lambda value: value, response: str="Permission Denied"):
+##     """
+##     Only complete the request if `predicate`(most_recent_group(`column`)) returns a truthy value
+## 
+##     :param column:
+##     :param predicate:
+##     :param response:
+##     :return:
+##     """
+##     def decorator(func):
+##         def inner(request):
+##             nonlocal column
+##             session = request.app["session"]
+##             group = functions.get_most_recent_group(session)
+##             if predicate(getattr(group, column)):
+##                 return func(request)
+##             return web.Response(status=403, text=response)
+##         return inner
+##     return decorator
+## 
+## 
+## def get_users_with_permission(app: Application, permission_names: Union[str, Sequence[str]]) -> Set:
+##     """
+##     Return the users who have any of the permissions in `permission_names`
+## 
+##     :param app:
+##     :param permission_names:
+##     :return:
+##     """
+##     rtn = set()
+##     if isinstance(permission_names, str):
+##         permission_names = (permission_names,)
+##     for user in functions.get_all_users(app["session"]):
+##         perms = get_user_permissions(app, user)
+##         for permission_name in permission_names:
+##             if permission_name in perms:
+##                 rtn.add(user)
+##     return rtn
+## 
+## 
+## def get_user_permissions(app: Application, user: Optional[User]) -> Set:
+##     """
+##     Return the permissions `user` has
+## 
+##     :param app:
+##     :param user:
+##     :return:
+##     """
+##     if user is None:
+##         return set()
+##     user_types = user.user_type.split("|")
+##     permissions = set()
+##     for user_type in user_types:
+##         if user_type:
+##             for permission, value in app["config"]["permissions"][user_type].items():
+##                 if value:
+##                     permissions.add(permission)
+##     return permissions
