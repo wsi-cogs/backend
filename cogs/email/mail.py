520f1694f8510000aeb0162b9ff58e1a0dc61333
Christopher Harrison <ch12@sanger.ac.uk>
2017-11-15 00:03:44 +0000

    WIP refactor of mailing API

--- cogs/email/mail.py
+++ cogs/email/mail.py
@@ -1,99 +1,168 @@
-import asyncio
-import smtplib
-from asyncio import get_event_loop
+"""
+Copyright (c) 2017 Genome Research Ltd.
+
+Authors:
+* Christopher Harrison <ch12@sanger.ac.uk>
+* Simon Beal <sb48@sanger.ac.uk>
+
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+# import aiofiles
+# from aiohttp.web import Application
+# from bleach import clean
+# from bs4 import BeautifulSoup, NavigableString, CData
+# from jinja2 import Environment, BaseLoader
+
+import atexit
+from smtplib import SMTP
 from concurrent.futures import ThreadPoolExecutor
-from email.mime.application import MIMEApplication
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from typing import Dict, Optional
+from typing import Dict, List, NamedTuple, Optional
 
-import aiofiles
-from aiohttp.web import Application
-from bleach import clean
-from bs4 import BeautifulSoup, NavigableString, CData
-from jinja2 import Environment, BaseLoader
+from jinja2 import FileSystemLoader, Environment, Template
 
-from cogs.db import functions
+from cogs.common.constants import ROTATION_TEMPLATE_IDS
+from cogs.db.models import User
+from .message import TemplatedEMail
 
 
-async def send_user_email(app: Application, user: str, template_name: str, attachments: Optional[Dict[str, str]]=None, **kwargs):
-    config = app["config"]["email"]
-    web_config = app["config"]["webserver"]
-
-    extra_content = ""
-    if kwargs.get("extension", False) is True:
-        extra_content = "The deadline has been extended to {{ new_deadline.strftime('%d/%m/%Y') }} due to not enough projects being submitted.<br><br><hr><br>"
-
-    contents = {}
-    if template_name in app["config"]["misc"]["email_whitelist"]:
-        template = functions.get_template_name(app["session"], template_name)
-        env = Environment(loader=BaseLoader).from_string(template.subject.replace("\n", ""))
-        contents["subject"] = env.render(config=config, user=user, web_config=web_config, **kwargs)
-        env = Environment(loader=BaseLoader).from_string(extra_content+template.content.replace("\n", ""))
-        contents["contents"] = env.render(config=config, user=user, web_config=web_config, **kwargs)
-    else:
-        for message_type in ("subject", "contents"):
-            async with aiofiles.open(f"cogs/email_templates/{template_name}_{message_type}.jinja2") as template_f:
-                env = Environment(loader=BaseLoader).from_string((await template_f.read()).replace("\n", ""))
-            rendered = env.render(config=config, user=user, web_config=web_config, **kwargs)
-            contents[message_type] = rendered
-
-    asyncio.ensure_future(send_email(to=user.email,
-                                     **contents,
-                                     attachments=attachments,
-                                     **config))
+class _Server(NamedTuple):
+    """ Server type """
+    host:str
+    port:int
 
 
-async def send_email(*, host: str, port: int, to: str, sender: str, subject: str, contents: str, attachments: Optional[Dict[str, bytes]]=None):
-    contents = contents + "<br><br>Best wishes,<br>gradoffice<br><a href='mailto:gradoffice@sanger.ac.uk'>gradoffice@sanger.ac.uk</a>"
-    loop = get_event_loop()
-    with ThreadPoolExecutor() as executor:
-        loop.run_in_executor(executor, _send_email, host, port, to, sender, subject, contents, attachments)
+def _email_from_db_template(template:str) -> TemplatedEMail:
+    """
+    Fetch rotation template from the database
+
+    :param template:
+    :return:
+    """
+
+class Mailer(object):
+    """ E-mail sender """
+    _server:_Server
+    _sender:str
+    _templates:Dict[str, Template]
+    _threadpool:ThreadPoolExecutor
+
+    def __init__(self, host:str, port:int, sender:str) -> None:
+        """
+        Constructor
+
+        :param host:
+        :param port:
+        :param sender:
+        :return:
+        """
+        self._server = _Server(host, port)
+        self._sender = sender
+
+        # Load the filesystem e-mail templates into memory
+        fs_loader = FileSystemLoader("cogs/email/templates")
+        fs_env = Environment(loader=fs_loader)
+        self._templates = {
+            template: fs_env.get_template(template)
+            for template in fs_loader.list_templates()
+        }
+
+        # We use a threadpool, rather than asyncio
+        self._threadpool = ThreadPoolExecutor()
+        atexit.register(self._threadpool.shutdown)
+
+    def send(self, user:User, template:str, attachments:Optional[List[str]] = None, **context) -> None:
+        """
+        Prepare the e-mail by template and context and submit it to the
+        threadpool to send to the user
+
+        :param user:
+        :param template:
+        :param attachments:
+        :return:
+        """
+        if template in ROTATION_TEMPLATE_IDS:
+            mail = _email_from_db_template(template)
+        else:
+            mail = TemplatedEMail(self._templates[f"{template}_subject.jinja2"],
+                                  self._templates[f"{template}_contents.jinja2"])
+
+        mail.sender = self._sender
+        mail.recipient = user.email
+
+        for attachment in attachments or []:
+            mail.add_attachment(attachment)
+
+        for k, v in context.items():
+            mail.set_context(k, v)
+
+        self._threadpool.submit(self._send_mail, mail)
+
+    def _send_mail(self, mail:TemplatedEMail) -> None:
+        """
+        Render the prepared e-mail and send
+
+        :param mail:
+        :return:
+        """
+        with SMTP(*self._server) as smtp:
+            smtp.send_message(mail.render())
 
 
-def _send_email(host: str, port: str, to: str, sender: str, subject: str, contents: str, attachments: Optional[Dict[str, bytes]]=None):
-    message = MIMEMultipart("alternative")
-    message["Subject"] = subject
-    message["From"] = sender
-    message["To"] = to
-
-    html = contents
-    text = get_text(html)
-    message.attach(MIMEText(text, 'plain'))
-    message.attach(MIMEText(html, 'html'))
-
-    if attachments is not None:
-        for name, data in attachments.items():
-            part = MIMEApplication(
-                data,
-                Name=name
-            )
-            part["Content-Disposition"] = f'attachment; filename="{name}"'
-            message.attach(part)
-
-    s = smtplib.SMTP(host, port)
-    s.sendmail(sender, to, message.as_string())
-    s.quit()
 
 
-def get_text(html: str):
-    soup = BeautifulSoup(html, "html.parser")
-    rtn = []
-    for descendant in soup.descendants:
-        if isinstance(descendant, (NavigableString, CData)):
-            parent = descendant.parent
-            if parent.name == "a":
-                rtn.append(f"{descendant} ({parent['href']})")
-            else:
-                rtn.append(str(descendant))
-        elif descendant.name == "br":
-            rtn.append("\n")
-    return "".join(rtn)
-
-
-def clean_html(html: str) -> str:
-    cleaned = clean(html,
-                    tags=['a', 'b', 'blockquote', 'code', 'em', 'i', 'li', 'ol', 'strong', 'ul', 'font', 'div', 'u', 'pre', 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'br', 'sub', 'sup', 'span'],
-                    attributes=['align', 'size', 'face', 'href', 'title', 'target'],
-                    strip=True)
-    return cleaned
+## async def send_user_email(app: Application, user: str, template_name: str, attachments: Optional[Dict[str, str]]=None, **kwargs):
+##     config = app["config"]["email"]
+##     web_config = app["config"]["webserver"]
+## 
+##     extra_content = ""
+##     if kwargs.get("extension", False) is True:
+##         extra_content = "The deadline has been extended to {{ new_deadline.strftime('%d/%m/%Y') }} due to not enough projects being submitted.<br><br><hr><br>"
+## 
+##     contents = {}
+##     if template_name in app["config"]["misc"]["email_whitelist"]:
+##         template = functions.get_template_name(app["session"], template_name)
+##         env = Environment(loader=BaseLoader).from_string(template.subject.replace("\n", ""))
+##         contents["subject"] = env.render(config=config, user=user, web_config=web_config, **kwargs)
+##         env = Environment(loader=BaseLoader).from_string(extra_content+template.content.replace("\n", ""))
+##         contents["contents"] = env.render(config=config, user=user, web_config=web_config, **kwargs)
+##     else:
+##         for message_type in ("subject", "contents"):
+##             async with aiofiles.open(f"cogs/email_templates/{template_name}_{message_type}.jinja2") as template_f:
+##                 env = Environment(loader=BaseLoader).from_string((await template_f.read()).replace("\n", ""))
+##             rendered = env.render(config=config, user=user, web_config=web_config, **kwargs)
+##             contents[message_type] = rendered
+## 
+## 
+## def get_text(html: str):
+##     soup = BeautifulSoup(html, "html.parser")
+##     rtn = []
+##     for descendant in soup.descendants:
+##         if isinstance(descendant, (NavigableString, CData)):
+##             parent = descendant.parent
+##             if parent.name == "a":
+##                 rtn.append(f"{descendant} ({parent['href']})")
+##             else:
+##                 rtn.append(str(descendant))
+##         elif descendant.name == "br":
+##             rtn.append("\n")
+##     return "".join(rtn)
+## 
+## 
+## def clean_html(html: str) -> str:
+##     cleaned = clean(html,
+##                     tags=['a', 'b', 'blockquote', 'code', 'em', 'i', 'li', 'ol', 'strong', 'ul', 'font', 'div', 'u', 'pre', 'p', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'br', 'sub', 'sup', 'span'],
+##                     attributes=['align', 'size', 'face', 'href', 'title', 'target'],
+##                     strip=True)
+##     return cleaned
---
+++ cogs/email/message.py
@@ -0,0 +1,107 @@
+"""
+Copyright (c) 2017 Genome Research Ltd.
+
+Authors:
+* Christopher Harrison <ch12@sanger.ac.uk>
+* Simon Beal <sb48@sanger.ac.uk>
+
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+import os.path
+from email.message import EmailMessage
+from typing import Any, Dict, List
+
+from jinja2 import Template
+
+from .constants import SIGNATURE
+
+
+class TemplatedEMail(object):
+    """ E-mail message generated from template"""
+    _sender:str
+    _recipient:str
+    _subject_template:Template
+    _body_template:Template
+    _attached_files:List[str]  # List of filenames, which are loaded on expansion
+    _context:Dict
+
+    def __init__(self, subject:Template, body:Template) -> None:
+        """
+        Construct e-mail from templates
+
+        :param subject:
+        :param body:
+        :return:
+        """
+        self._subject_template = subject
+        self._body_template = body
+        self._attached_files = []
+        self._context = {}
+
+    def render(self) -> EmailMessage:
+        """ Render the e-mail message in full and send it """
+        assert self._recipient and self._sender
+
+        mail = EmailMessage()
+        mail["To"] = self._recipient
+        mail["From"] = self._sender
+
+        mail["Subject"] = self._subject_template.render(**self._context)
+
+        html_body = self._body_template.render(**self._context) + SIGNATURE
+        text_body = _render_html_as_text(html_body)
+
+        mail.set_content(text_body)
+        mail.add_alternative(html_body, subtype="html")
+
+        for attachment in self._attached_files:
+            with open(attachment, "rb") as data:
+                mail.add_attachment(data.read(),
+                                    filename=os.path.basename(attachment),
+                                    maintype="application",
+                                    subtype="octet-stream")
+        return mail
+
+    @property
+    def sender(self) -> str:
+        return self._sender
+
+    @sender.setter
+    def sender(self, address:str) -> None:
+        self._sender = address
+
+    @property
+    def recipient(self) -> str:
+        return self._recipient
+
+    @recipient.setter
+    def recipient(self, address:str) -> None:
+        self._recipient = address
+
+    def add_attachment(self, attachment:str) -> None:
+        self._attached_files.append(attachment)
+
+    def set_context(self, key:str, value:Any) -> None:
+        self._context[key] = value
+
+
+def _render_html_as_text(html:str) -> str:
+    """
+    Convert HTML into plaintext
+
+    :param html"
+    :return:
+    """
+    return "foo"
