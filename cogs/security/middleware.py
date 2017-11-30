dbee40fbd57860c634edaf02f6c88c59da2043ec
Christopher Harrison <ch12@sanger.ac.uk>
2017-11-30 11:57:57 +0000

    Docstring clean up

--- cogs/security/middleware.py
+++ cogs/security/middleware.py
@@ -2,8 +2,8 @@
 Copyright (c) 2017 Genome Research Ltd.
 
 Authors:
-* Simon Beal <sb48@sanger.ac.uk>
 * Christopher Harrison <ch12@sanger.ac.uk>
+* Simon Beal <sb48@sanger.ac.uk>
 
 This program is free software: you can redistribute it and/or modify it
 under the terms of the GNU Affero General Public License as published by
--- cogs/security/model.py
+++ cogs/security/model.py
@@ -2,7 +2,6 @@
 Copyright (c) 2017 Genome Research Ltd.
 
 Authors:
-* Simon Beal <sb48@sanger.ac.uk>
 * Christopher Harrison <ch12@sanger.ac.uk>
 
 This program is free software: you can redistribute it and/or modify it
