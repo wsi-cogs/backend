dd0d6d0c63395a7a55d7db1f6257b9f86715da0e
Christopher Harrison <ch12@sanger.ac.uk>
2017-12-11 16:42:27 +0000

    (Partially) refactored root route
    TODO/FIXME Fix/remove the state changing bits, which are currently
    commented out for sanity's sake

--- cogs/routes/user_page.py
+++ cogs/routes/user_page.py
@@ -1,47 +1,73 @@
+"""
+Copyright (c) 2017 Genome Research Ltd.
+
+Authors:
+* Simon Beal <sb48@sanger.ac.uk>
+* Christopher Harrison <ch12@sanger.ac.uk>
+
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero
+General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+"""
+
 from typing import Dict
 
-from aiohttp.web_request import Request
+from aiohttp.web import Request
 from aiohttp_jinja2 import template
-
-from cogs.db.functions import get_projects_supervisor, get_user_id, get_student_projects, get_navbar_data, get_projects_cogs, \
-    set_project_can_mark, set_group_attributes, sort_by_attr, get_user_cookies
-from permissions import get_user_permissions
 
 
 @template("user_page.jinja2")
 async def user_page(request: Request) -> Dict:
     """
-    Get the page for the user.
-    If they own projects, show them including legacy projects.
-    If they are part of projects, show them.
-    If they are in the process for signing up for projects, put them at the start
+    Set the template context for the user. Show the projects they:
+    * Own (including legacy projects);
+    * Are involved with;
+    * Are in the process of signing up for, front loaded
 
     :param request:
     :return:
     """
-    cookies = request.cookies
-    session = request.app["session"]
-    user = get_user_id(request.app, cookies)
-    rtn = {
-        "user": user,
-        "cur_option": "cogs",
-        **get_navbar_data(request)
-    }
-    if user:
-        rtn["first_option"] = user.first_option
-        rtn["second_option"] = user.second_option
-        rtn["third_option"] = user.third_option
-    permissions = get_user_permissions(request.app, user)
-    if "review_other_projects" in permissions:
-        rtn["review_list"] = series_list = get_projects_cogs(request.app, cookies)
-        for series in series_list:
-            for project in series:
-                set_project_can_mark(request.app, cookies, project)
-            sort_by_attr(series, "can_mark")
-    if "join_projects" in permissions:
-        rtn["project_list"] = get_student_projects(request.app, cookies)
-    if "create_projects" in permissions:
-        rtn["series_list"] = series_list = get_projects_supervisor(session, get_user_cookies(request.app, request.cookies))
-        for series in series_list:
-            set_group_attributes(request.app, cookies, series)
-    return rtn
+    db = request["db"]
+    user = request["user"]
+    navbar_data = request["navbar"]
+
+    data = {
+        "user":          user,
+        "cur_option":    "cogs",
+        "first_option":  user.first_option,
+        "second_option": user.second_option,
+        "third_option":  user.third_option,
+        **navbar_data}
+
+    if user.role.review_other_projects:
+        data["review_list"] = series_list = db.get_projects_by_cogs_marker(user)
+
+        # TODO Refactor this...or remove it: It looks like it's doing
+        # dangerous things!
+
+        # for series in series_list:
+        #     for project in series:
+        #         set_project_can_mark(request.app, cookies, project)
+        #     sort_by_attr(series, "can_mark")
+
+    if user.role.join_projects:
+        data["project_list"] = db.get_projects_by_student(user)
+
+    if user.role.create_projects:
+        data["series_list"] = series_list = db.get_projects_by_supervisor(user)
+
+        # TODO/FIXME Dragons be here! Remove this if possible
+
+        # for series in series_list:
+        #     set_group_attributes(request.app, cookies, series)
+
+    return data
