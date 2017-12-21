c2e7914750aa0ad8f0b66e3686d79c9c3165e22b
Christopher Harrison <ch12@sanger.ac.uk>
2017-12-21 14:45:18 +0000

    Basic test for adding scheduled jobs

--- test/scheduler/test_scheduler.py
+++ test/scheduler/test_scheduler.py
@@ -21,17 +21,35 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 import unittest
 from unittest.mock import MagicMock, patch
+from datetime import date
 
 from cogs.scheduler import Scheduler
+from cogs.scheduler.constants import GROUP_DEADLINES
 
+
+_MOCK_SCHEDULER_ARGS = (MagicMock(),) * 3
 
 @patch("cogs.scheduler.scheduler.AsyncIOScheduler", spec=True)
 class TestScheduler(unittest.TestCase):
     def test_constructor(self, mock_scheduler):
-        s = Scheduler(MagicMock(), MagicMock(), MagicMock())
+        s = Scheduler(*_MOCK_SCHEDULER_ARGS)
 
         mock_scheduler.assert_called_once()
         s._scheduler.start.assert_called_once()
+
+    def test_schedule_deadline(self, mock_scheduler):
+        s = Scheduler(*_MOCK_SCHEDULER_ARGS)
+        self.assertRaises(AssertionError, s.schedule_deadline, date.today(), "foobar", None)
+
+        for deadline_id, deadline in GROUP_DEADLINES.items():
+            s._scheduler.add_job.reset_mock()
+            s.schedule_deadline(date.today(), deadline_id, MagicMock())
+            s._scheduler.add_job.assert_called_once()
+
+            s._scheduler.add_job.reset_mock()
+            s.schedule_deadline(date.today(), deadline_id, MagicMock(), to="foo@bar")
+            calls = s._scheduler.add_job.call_count
+            self.assertEqual(calls, 1 + len(deadline.pester_times))
 
 
 if __name__ == "__main__":
