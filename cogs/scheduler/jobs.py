wsi-cogs/backend

commit acd31bafe05fff9150c225fc31bce7d2aa19dc5d

Use one job with multiple triggers for reminders
Previously, one job would be scheduled for each "pester point" of a
deadline. Combined with the very long misfire_grace_time, this would
result in several reminders being sent simultaneously if they were
scheduled in the past, and in that case the notification would also
state the wrong number of days left (since the delta was hardcoded).

Now, each deadline's reminders are contained within a single job, which
is triggered at several predefined dates relative to the deadline. The
offset between the job and the deadline is calculated when the reminder
is sent, and if two reminders are scheduled to run simultaneously (e.g.
the 14-day and 7-day reminders), only the most recent reminder will be
sent (see #12).

Fixes #7.

cogs/scheduler/jobs.py
@@ -159,7 +159,12 @@ async def grace_deadline(scheduler:"Scheduler", project_id:int) -> None:
             project_id = project.id)
 
 
+# TODO: once there are no instances with an old-style pester scheduled, this job can be removed.
 async def pester(scheduler: "Scheduler", deadline: str, delta_time: int, group_series: int, group_part: int, *recipients: int) -> None:
+    return reminder(scheduler, deadline, group_series, group_part, *recipients)
+
+
+async def reminder(scheduler: "Scheduler", deadline: str, group_series: int, group_part: int, *recipients: int) -> None:
     """
     Remind users about a specific deadline
     """
@@ -171,18 +176,21 @@ async def pester(scheduler: "Scheduler", deadline: str, delta_time: int, group_s
     else:
         users = db.get_users_by_permission(*GROUP_DEADLINES[deadline].pester_permissions)
 
-    # Get the group we are pestering users about
-    group = db.get_project_group(group_series, group_part)
+    # Get the group we are pestering users about (with a slightly
+    # awkward construction to make mypy happy).
+    maybe_group = db.get_project_group(group_series, group_part)
+    assert maybe_group is not None, \
+        f"Reminder fired for {group_series}-{group_part}, which doesn't exist!"
+    group = maybe_group
 
     _predicate = GROUP_DEADLINES[deadline].pester_predicate
 
     # mypy has poor support for functools.partial, so we don't use it here.
     def predicate(user: User):
-        assert group is not None, \
-            f"Reminder fired for {group_series}-{group_part}, which doesn't exist!"
         return _predicate(user, rotation=group)
 
     template = GROUP_DEADLINES[deadline].pester_template.format(group=group)
+    delta_time = (getattr(group, deadline) - date.today()).days
     for user in filter(predicate, users):
         mail.send(user,
                   template,

cogs/scheduler/scheduler.py
@@ -26,6 +26,7 @@ from typing import ClassVar, List
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.date import DateTrigger
+from apscheduler.triggers.combining import OrTrigger
 from pytz import utc
 
 from cogs.common import logging
@@ -111,17 +112,26 @@ class Scheduler(logging.LogWriter):
             replace_existing = True)
 
         # Pester points
-        # The pester job contains logic to ensure that tasks are only
+        # The reminder job contains logic to ensure that tasks are only
         # completed if the appropriate conditions are met, otherwise
         # they're effectively no-ops
+        self._scheduler.add_job(
+            self._job,
+            id=f"reminders_for_{job_id}",
+            trigger=OrTrigger([
+                DateTrigger(run_date=schedule_time - timedelta(days=delta_day))
+                for delta_day in GROUP_DEADLINES[deadline].pester_times
+            ]),
+            args=("reminder", deadline, group.series, group.part, *recipients),
+            replace_existing=True,
+            coalesce=True,
+        )
+        # Remove existing old-style pester jobs
+        # TODO: remove this once there are no instances with scheduled pesters
         for delta_day in GROUP_DEADLINES[deadline].pester_times:
-            pester_job_id = f"pester_{delta_day}_{job_id}"
-            pester_time = schedule_time - timedelta(days=delta_day)
-            self._scheduler.add_job(self._job,
-                trigger          = DateTrigger(run_date=pester_time),
-                id               = pester_job_id,
-                args             = ("pester", deadline, delta_day, group.series, group.part, *recipients),
-                replace_existing = True)
+            existing_job = self._scheduler.get_job(f"pester_{delta_day}_{job_id}")
+            if existing_job is not None:
+                self._scheduler.remove_job(f"pester_{delta_day}_{job_id}")
 
     def schedule_user_deadline(self, when:date, deadline, suffix, *args, **kwargs):
         assert deadline in USER_DEADLINES

commit b5a8908afa80a1fd48787ba802e096e19b9144c6

Fix old-style pester job
Bug introduced in acd31ba ("Use one job with multiple triggers for
reminders").

cogs/scheduler/jobs.py
@@ -169,7 +169,7 @@ async def grace_deadline(scheduler:"Scheduler", project_id:int) -> None:
 
 # TODO: once there are no instances with an old-style pester scheduled, this job can be removed.
 async def pester(scheduler: "Scheduler", deadline: str, delta_time: int, group_series: int, group_part: int, *recipients: int) -> None:
-    reminder(scheduler, deadline, group_series, group_part, *recipients)
+    await reminder(scheduler, deadline, group_series, group_part, *recipients)
 
 
 async def reminder(scheduler: "Scheduler", deadline: str, group_series: int, group_part: int, *recipients: int) -> None: